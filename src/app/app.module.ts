f60b0f0a0183f1352967cc4d28e1214fd926cec2
Gerd <gerdemmanuel@web.de>
2021-10-27 21:26:27 +0200

    added new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BgbNavigationComponent } from './components/bgb-navigation/bgb-navigation.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BgbNavigationComponent} from './components/bgb-navigation/bgb-navigation.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-import { AppRoutingModule } from './shared/app-routing.module';
-import { BgbHomeSiteComponent } from './sites/bgb-home-site/bgb-home-site.component';
-import { BgbMembershipComponent } from './sites/bgb-membership/bgb-membership.component';
+import {AppRoutingModule} from './shared/app-routing.module';
+import {BgbHomeSiteComponent} from './sites/bgb-home-site/bgb-home-site.component';
+import {BgbMembershipComponent} from './sites/bgb-membership/bgb-membership.component';
 import {MatTabsModule} from "@angular/material/tabs";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import { BgbCarouselComponent } from './components/bgb-carousel/bgb-carousel.component';
+import {BgbCarouselComponent} from './components/bgb-carousel/bgb-carousel.component';
 import {IvyCarouselModule} from "angular-responsive-carousel";
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
-import { BgbFooterComponent } from './components/bgb-footer/bgb-footer.component';
+import {BgbFooterComponent} from './components/bgb-footer/bgb-footer.component';
 import {NgImageSliderModule} from "ng-image-slider";
 import {BgbEventsSiteComponent} from "./sites/bgb-events/bgb-events-site.component";
 import {BgbShopSiteComponent} from "./sites/bgb-shop/bgb-shop-site.component";
 import {MatGridListModule} from "@angular/material/grid-list";
-import { BgbSchwimmnudelnComponent } from './sites/bgb-schwimmnudeln/bgb-schwimmnudeln.component';
-import { BgbContactComponent } from './sites/bgb-contact/bgb-contact.component';
+import {BgbSchwimmnudelnComponent} from './sites/bgb-schwimmnudeln/bgb-schwimmnudeln.component';
+import {BgbContactComponent} from './sites/bgb-contact/bgb-contact.component';
 import {MatListModule} from "@angular/material/list";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {PdfViewerModule} from "ng2-pdf-viewer";
 
 @NgModule({
   declarations: [
@@ -41,35 +42,36 @@ import {MatTooltipModule} from "@angular/material/tooltip";
     BgbSchwimmnudelnComponent,
     BgbContactComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatButtonModule,
-        AppRoutingModule,
-        MatTabsModule,
-        IvyCarouselModule,
-        // ngx-translate and the loader module
-        HttpClientModule,
-        NgImageSliderModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
-        MatCardModule,
-        MatDividerModule,
-        MatGridListModule,
-        MatListModule,
-        MatTooltipModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatTabsModule,
+    IvyCarouselModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    NgImageSliderModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    MatCardModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatListModule,
+    MatTooltipModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
